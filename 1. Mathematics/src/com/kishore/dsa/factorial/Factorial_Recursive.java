2c354b9bf2a5169c5e62f087af40030b11fd466d
kishore5464 <kishorekumar5464@gmail.com>
2022-01-29 22:53:58 +0530

    Greatest Common Divisor

--- 1. Mathematics/src/com/kishore/dsa/factorial/Factorial_Recursive.java
+++ 1. Mathematics/src/com/kishore/dsa/factorial/Factorial_Recursive.java
@@ -1,6 +1,7 @@
 package com.kishore.dsa.factorial;
 
 public class Factorial_Recursive {
+	
 	public static void main(String[] args) {
 		int fact = factorial(5);
 		System.out.println(fact);
---
+++ 1. Mathematics/src/com/kishore/dsa/gcd/GreatestCommonDivisor.java
@@ -0,0 +1,15 @@
+package com.kishore.dsa.gcd;
+
+/***
+ * GCD -> Greatest Common Divisor
+ * E.g) x = 4, y = 6;
+ * 		We need to find the greatest number which will divide both x and y.
+ * 		result = 2
+ *
+ */
+public class GreatestCommonDivisor {
+
+	public static void main(String[] args) {
+		
+	}
+}
