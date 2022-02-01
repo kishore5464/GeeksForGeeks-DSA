040c783b106814b8385ba6d3c7d30ddbb5e77de6
kishore5464 <kishorekumar5464@gmail.com>
2022-02-01 23:11:24 +0530

    Prime Factors with Optimized Solution

---
+++ 1. Mathematics/src/com/kishore/dsa/primeFactors/PrimeFactors_Optimized.java
@@ -0,0 +1,27 @@
+package com.kishore.dsa.primeFactors;
+
+public class PrimeFactors_Optimized {
+
+	public static void main(String[] args) {
+		primeFactors(12);
+	}
+
+	private static void primeFactors(int n) {
+
+		if (n <= 1) {
+			return;
+		}
+
+		for (int i = 2; i * i <= n; i++) {
+			while (n % i == 0) {
+				System.out.println(i);
+				n = n / i;
+			}
+		}
+
+		if (n > 1) {
+			System.out.println(n);
+		}
+	}
+
+}
